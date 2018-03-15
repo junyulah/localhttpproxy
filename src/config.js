9a1089883d53e1e92f030b8410de4cc0acf105dc
loveKino <842914439@qq.com>
2018-03-15 18:25:36 +0800

    config and host proxy

---
+++ src/config.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const {promisify} = require('es6-promisify');
+const path = require('path');
+
+const readFile = promisify(fs.readFile);
+const stat = promisify(fs.stat);
+
+const isWindow = () => process.platform === 'win32';
+
+const getUserHome = () => {
+    return process.env[isWindow() ? 'USERPROFILE' : 'HOME'];
+};
+
+const existsFile = (filePath) => {
+    return new Promise((resolve) => {
+        stat(filePath).then((statObj) => {
+            resolve(statObj.isFile());
+        }).catch(() => {
+            resolve(false);
+        });
+    });
+};
+
+const getConfig = () => {
+    const configPath = path.join(getUserHome(), 'lhp.config.json');
+    return existsFile(configPath).then((has) => {
+        if (!has) {
+            return {};
+        } else {
+            return readFile(configPath, 'utf-8').then((txt) => {
+                return JSON.parse(txt);
+            });
+        }
+    });
+};
+
+module.exports = {
+    getConfig
+};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
 const http = require('http');
 const url = require('url');
-const logHttp = require('./logHttp');
 const uuidv4 = require('uuid/v4');
-const log = console.log; // eslint-disable-line
+const {
+    getConfig
+} = require('./config');
 
-const forward = (req, res) => {
+const getForwardOption = (req) => {
     const urlObject = url.parse(req.url, false);
     const targetHost = urlObject.host || req.headers.host;
-
     const hostname = targetHost.split(':')[0].trim();
     const port = Number((targetHost.split(':')[1] || '').trim() || 80);
-
-    const httpReq = http.request({
+    return {
         host: hostname,
         port,
         method: req.method,
         path: urlObject.path,
-        headers: req.headers
-    }, (httpRes) => {
+        headers: Object.assign({}, req.headers)
+    };
+};
+
+const proxyHost = (req, targetHost) => {
+    const urlObject = url.parse(req.url, false);
+    const hostname = targetHost.split(':')[0].trim();
+    const port = Number((targetHost.split(':')[1] || '').trim() || 80);
+
+    return {
+        host: hostname,
+        port,
+        method: req.method,
+        path: urlObject.path,
+        headers: Object.assign({}, req.headers, {
+            referer: urlObject.protocol + '//' + targetHost + '/',
+            host: targetHost
+        })
+    };
+};
+
+const pipeRequest = (reqOptions, req, res) => {
+    const httpReq = http.request(reqOptions, (httpRes) => {
         res.statusCode = httpRes.statusCode;
         for (const name in httpRes.headers) {
             res.setHeader(name, httpRes.headers[name]);
@@ -42,31 +62,56 @@ const forward = (req, res) => {
 
 const watchers = {};
 
-const server = http.createServer((req, res) => {
-    if (req.headers.host !== '127.0.0.1:3130') {
-        logHttp(req, watchers);
-        forward(req, res);
-    } else { // handler for current server request
-        if (req.url === '/live') {
-            res.end('I am alive');
-        } else if (req.url === '/shutdown') {
-            res.end('about to shutdown');
-            process.exit(0);
-        } else if (req.url === '/log') {
-            const id = uuidv4();
-            watchers[id] = {
-                res,
-                req
-            };
-            res.on('close', () => {
-                delete watchers[id];
-            });
-        } else {
-            res.end('no command for this');
-        }
-    }
-});
+const logInHttp = (req) => {
+    console.log(req.url); // eslint-disable-line
 
-server.listen(3130, '127.0.0.1', () => {
-    log(`server start at ${server.address().port}`);
+    for (const id in watchers) {
+        const watcher = watchers[id];
+        watcher.res.write(req.url + '\n');
+    }
+};
+
+getConfig().then((config) => {
+    const server = http.createServer((req, res) => {
+        const urlObject = url.parse(req.url, false);
+
+        if (req.headers.host !== '127.0.0.1:3130') {
+            logInHttp(req, req.url);
+            const hostRule = config.proxy && config.proxy.host && config.proxy.host[req.headers.host];
+            // check host
+            if (hostRule) {
+                if (!hostRule.pathReg || new RegExp(hostRule.pathReg).test(urlObject.path)) {
+                    logInHttp(req, `[proxy host]: ${req.headers.host} => ${hostRule.targetHost}`);
+                    return pipeRequest(proxyHost(req, hostRule.targetHost), req, res);
+                }
+            }
+
+            logInHttp(req, `[forward] to ${req.headers.host}`);
+            // forward
+            return pipeRequest(getForwardOption(req), req, res);
+        } else { // handler for current server request
+            if (req.url === '/live') {
+                return res.end('I am alive');
+            } else if (req.url === '/shutdown') {
+                res.end('about to shutdown');
+                return process.exit(0);
+            } else if (req.url === '/log') {
+                const id = uuidv4();
+                watchers[id] = {
+                    res,
+                    req
+                };
+                res.on('close', () => {
+                    delete watchers[id];
+                });
+                return;
+            } else {
+                return res.end('no command for this');
+            }
+        }
+    });
+
+    server.listen(3130, '127.0.0.1', () => {
+        console.log(`server start at ${server.address().port}`); // eslint-disable-line
+    });
 });
